97954b0ea0308ba9adba64a56c7501daeaeb0751
PAMALSAHAN <p.s.jayawickrama@gmail.com>
2020-03-18 22:06:59 +0530

    init entity framework done

---
+++ BookListRazor/model/AppDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookListRazor.model
+{
+    public class AppDbContext :DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> opt): base(opt) // parameter is needed for dependancy injection
+        {
+                
+        }
+
+        // need to add book model to the database
+        public DbSet<Book> BookModelProperty { get; set; }  //table name eka thamai BookModelProperty meka
+
+    }
+}
